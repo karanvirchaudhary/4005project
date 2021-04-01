79a4b4aa5aa5f4366733e3aa1d71ff09e8c17b1f
tanvir1990 <pavel.mhp@gmail.com>
2021-04-01 10:49:23 -0400

    one semicolon missing

--- src/Simulation.java
+++ src/Simulation.java
@@ -65,7 +65,7 @@ public class Simulation {
         System.out.println(lambdaWs1);
 
 
-        ArrayList<Buffer> inspectrOneBuffers = new ArrayList<>()
+        ArrayList<Buffer> inspectrOneBuffers = new ArrayList<>();
 
         Buffer bufferOne = new Buffer(); //buffer for inspector one and ws1
         Buffer bufferTwo = new Buffer();
