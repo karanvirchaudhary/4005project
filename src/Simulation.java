12bdf3e1186f60412c5927fdb6b3b0bf84669e24
abhiramj4 <abhisanthosh@cmail.carleton.ca>
2021-03-31 23:25:32 -0400

    data plus stuff

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Simulation {
+
+    public double getNext(Random random, Double lambda) {
+        return  Math.log(1-random.nextDouble())/(-lambda);
+    }
+
     public static void main(String[] args){
-        System.out.println("hello world!");
+
     }
 }
