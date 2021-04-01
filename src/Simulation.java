647ebf4d1829d50a06eba789c8861afb57e7f448
abhiramj4 <abhisanthosh@cmail.carleton.ca>
2021-03-31 23:39:02 -0400

    update

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,12 +1,29 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Simulation {
 
-    public double getNext(Random random, Double lambda) {
+    //10 replications with 300 items each
+    private ArrayList<ArrayList<Double>> servinsp1 = new ArrayList<ArrayList<Double>>();
+    private ArrayList<ArrayList<Double>> servinsp22 = new ArrayList<ArrayList<Double>>();
+    private ArrayList<ArrayList<Double>> servinsp23 = new ArrayList<ArrayList<Double>>();
+
+    private ArrayList<ArrayList<Double>> ws1 = new ArrayList<ArrayList<Double>>();
+    private ArrayList<ArrayList<Double>> ws2 = new ArrayList<ArrayList<Double>>();
+    private ArrayList<ArrayList<Double>> ws3 = new ArrayList<ArrayList<Double>>();
+
+    public double getExponential(Random random, Double lambda) {
         return  Math.log(1-random.nextDouble())/(-lambda);
     }
 
+    public void readFile(String fileName){
+        
+    }
+
     public static void main(String[] args){
+
+
+
 
     }
 }
