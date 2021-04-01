0c5d94ab53ac5cf875378544019a3e42eccb3d26
Karanvir Chaudhary <karanvir@hotmail.ca>
2021-03-31 23:07:21 -0400

    Added the files once again

--- src/Inspector.java
+++ src/Inspector.java
@@ -1,2 +1,12 @@
-public class Inspector {
+import java.util.ArrayList;
+
+public class Inspector{
+    private ArrayList<Component> inventory;
+    private boolean blocked;
+    private int ID;
+
+    public Inspector(boolean blocked, int ID){
+        this.ID = ID;
+        this.blocked = blocked;
+    }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,2 +1,5 @@
 public class Simulation {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
 }
